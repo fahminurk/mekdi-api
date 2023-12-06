6776a9c1b5bab880db8801964d90f57ddbab8760
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-06 21:05:24 +0700

    first commit

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/lib/prisma/prisma.service';
-import { Prisma } from '@prisma/client';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  create(createAccountDto: Prisma.UserCreateInput) {
-    return this.prismaService.user.create({ data: createAccountDto });
+  create(createAuthDto: CreateAuthDto) {
+    return 'This action adds a new auth';
   }
 
   findAll() {
-    return this.prismaService.user.findMany();
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
   }
 }
